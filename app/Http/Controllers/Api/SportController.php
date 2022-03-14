09d33d09fe2d1a153b0c66e5644a8279aabcbaf3
philzilla <bonjour@philippeyim.fr>
2022-03-14 10:54:32 +0100

    Ajout POST

--- app/Http/Controllers/Api/SportController.php
+++ app/Http/Controllers/Api/SportController.php
@@ -2,13 +2,13 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
 use App\Models\Sport;
-// use Illuminate\Http\Request;
-
+use Illuminate\Http\Request;
 use App\Http\Requests\SportRequest;
-use App\Http\Resources\SportResource;
 
+use App\Http\Controllers\Controller;
+use App\Http\Resources\SportResource;
+use Validator;
 
 class SportController extends Controller
 {
@@ -18,13 +18,61 @@ class SportController extends Controller
         //
     }
 
-  
-    // POST Ajouter un sport
+    /*************************************************************************/
+    /**** Méthode POST*****/
+    /*************************************************************************/
+    /*
+    // Méthode 1 - POST Ajouter un sport
     public function store(SportRequest $request)
     {
         $sport = Sport::create($request->validated());
         return new SportResource($sport);
     }
+    */
+    
+    /*
+    // Méthode 2 - POST Ajouter un sport
+    public function store(Request $request)
+    {
+      $request->validate([ 
+        'name'=>'required', 
+      ]);
+      
+      $sport = Sport::create([
+        'name' => $request->name,
+      ]);
+
+      // 2eme façon de faire
+      // $input = $request->all();
+      // $sport = Sport::create($input);
+  
+      return new SportResource($sport);
+    }
+    */
+
+    
+    // Méthode 3 - POST Ajouter un sport
+    public function store(Request $request)
+    {
+      $input = $request->all();
+   
+      $validator = Validator::make($input, [
+          'name' => 'required',
+      ]);
+
+      if($validator->fails()){
+          return $this->sendError('Validation Error.', $validator->errors());       
+      }
+
+      $sport = Sport::create($input);
+      
+      return response()->json([
+        'status' => 'Success',
+        'data' => $sport,
+      ]);
+
+    }
+    
 
     
     public function show(Sport $sport)
--- app/Http/Resources/SportResource.php
+++ app/Http/Resources/SportResource.php
@@ -6,14 +6,17 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class SportResource extends JsonResource
 {
-    /**
-     * Transform the resource into an array.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
-     */
+
     public function toArray($request)
     {
         return parent::toArray($request);
+
+        /****** Alternative *******
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+        ];
+        */
+        
     }
 }
